4e5f1dbf9d1b25a6753b2762440aa83596fb1850
Shelley Clem <shelley@shelleys-mbp.attlocal.net>
2021-05-12 15:33:51 -0400

    fixed fetch, added styling

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 const baseURL = 'https://cat-fact.herokuapp.com/facts/random';
 const factButton = document.getElementById('button')
 
-console.log(factButton);
+//console.log(factButton);
 let catFactOTM = document.querySelector('#catFactOTM');
-factButton.addEventListener("submit", fetchResults);
+//factButton.addEventListener("submit", fetchResults); (lines 6 and 7 doing the same thing)
+factButton.addEventListener("click", ()=> {
+    fetchResults()
+});
 
 
-function fetchResults(event) {
+function fetchResults() {
     console.log('hit');
     fetch(baseURL)
     .then(function (response) {
@@ -16,23 +19,17 @@ function fetchResults(event) {
         console.log(data);
         display(data)
     })
-    event.preventDefault()
+    .catch(function(err) {
+        console.log("Are you kitten me? There's been a cat-astrophic error. Try again meow.");
+    });
+
+    //event.preventDefault()
 }
 
-const display = (data) => {
+let display = (data) => {
     console.log(data.text);
     let catfact = data.text;
     //catFactOTM= catfact
-    catfact = catFactOTM.innerText;
+    catFactOTM.textContent = catfact;
+    console.log(catFactOTM);
 }
-
-
-
-
-// .catch(function(err) {
-//     console.log("Are you kitten me? There's been a cat-astrophic error. Try again meow.");
-// });
-
-
-//Event listener for button click (e) short for event
-    //display text on a cat photos saved in assets 
